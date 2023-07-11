f558bede69c19731b85edc5c38fbd6f6aef94082
Pranjal Sharma <sharmapranjal23a@gmail.com>
2023-07-11 18:22:31 +0530

    Add a user controller and route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express= require('express');
 
 const router = express.Router();
+
 const homeController= require('../controllers/home_controller');
 
-// console.log('Router loaded');
-
 router.get('/', homeController.home);
+router.use('/user', require('./users'));
 module.exports= router;
