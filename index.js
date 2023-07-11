7fca0448018c6d2bde374dc53c8249a4e62a2310
Pranjal Sharma <sharmapranjal23a@gmail.com>
2023-07-11 18:33:06 +0530

    EJS added

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express= require('express');
 const app =express();
 
+// defining the port number
 const port= 8000;
 
 // Use express router
 app.use('/', require('./routes/index'));
 
+// Set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+    
+// Connecting to server
 app.listen(port, function(err){
     if (err) {
         console.log(`Error in running the server: ${err}`);
